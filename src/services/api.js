a2e9f9789af5acc4823b0e80b003acb0c9916783
verma-shu6ham <verma.shu6ham@gmail.com>
2025-10-05 00:17:26 +0530

    proxy api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 import { handleApiError } from '../utils/errorHandler'
 
-const API_URL = `${process.env.REACT_APP_API_URL}/api`
+const API_URL = `/api`
 
 const api = axios.create({
   baseURL: API_URL,
